1b48728ddc851efc2b0403e6c92f14f217db0912
Nick Hosman <nickhosman@gmail.com>
2024-06-19 22:28:38 -0700

    refactor frontend with sveltekit and tailwind

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,13 +1,15 @@
-// Tauri doesn't have a Node.js server to do proper SSR
-// so we will use adapter-static to prerender the app (SSG)
-// See: https://beta.tauri.app/start/frontend/sveltekit/ for more info
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-  },
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
