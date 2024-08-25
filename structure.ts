a9eaac771153d13ceae2e6a5ebdf226936fe77e7
Nick <nickhosman@gmail.com>
2024-08-25 12:58:27 -0700

    character framework

---
+++ structure.ts
@@ -0,0 +1,56 @@
+enum profType {
+    LANGUAGE,
+    TOOL,
+    SKILL,
+    SAVING_THROW,
+    ARMOR,
+    WEAPON,
+};
+
+type Proficiency = {
+    type: profType;
+    name: string;
+};
+
+type Feature = {
+    name: string;
+    description: string;
+};
+
+type Race = {
+    name: string;
+    description: string;
+    features: Feature[];
+    proficiencies: Proficiency[];
+};
+
+enum hitDie {
+    D4 = 4,
+    D6 = 6,
+    D8 = 8,
+    D10 = 10,
+    D12 = 12,
+};
+
+type Class = {
+    name: string;
+    description: string;
+    features: Feature[];
+    proficiencies: Proficiency[];
+    hitDie: hitDie;
+};
+
+
+type Background = {
+    name: string;
+    description: string;
+    proficiencies: Proficiency[];
+    features: Feature[];
+};
+
+type Character = {
+    name: string;
+    race: Race;
+    classes: Class[];
+    level: number;
+};
