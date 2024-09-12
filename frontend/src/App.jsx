d54f8dad0668b1553f10a31ba667b4de927f68b2
Nick <nickhosman@gmail.com>
2024-09-11 23:33:50 -0700

    skeleton homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,14 @@ function App() {
   return (
     <>
       <h1>Welcome to Campaign Kit!</h1>
+      <ul>
+        <li>
+          <a href="/campaigns">Campaigns</a>
+          <a href="/campaigns/new">New Campaign</a>
+          <a href="/characters" >Characters</a>
+          <a href="/characters/new" >New Character</a>
+        </li>
+      </ul>
     </>
   )
 }
