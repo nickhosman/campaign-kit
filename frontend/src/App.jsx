9b08897d0012c2eee8105f6b2eb9571e7001c11f
Nick <nickhosman@gmail.com>
2024-09-11 23:37:23 -0700

    basic styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     <>
       <h1>Welcome to Campaign Kit!</h1>
       <ul>
-        <li>
+        <li className="flex w-full h-12 bg-orange-800 text-white gap-4">
           <a href="/campaigns">Campaigns</a>
           <a href="/campaigns/new">New Campaign</a>
           <a href="/characters" >Characters</a>
